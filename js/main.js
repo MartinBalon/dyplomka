1cea0d3208646d862d90fac108900d4f91d2d5b3
Martin Balon <martin.balon@techfides.cz>
2022-02-12 19:48:11 +0100

    finished styling for tablets and small laptops

--- js/main.js
+++ js/main.js
@@ -14,14 +14,12 @@ let linkClicked = false;
 
 const toggleNavigation = () => {
 	if (!isMenuActive) {
-		menu.style.transform = "translate(0%)";
+		menu.classList.add("showNavigation");
 		isMenuActive = true;
-		document.body.style.overflow = "hidden";
 		animateCloseMenuButton(true);
 	} else {
-		menu.style.transform = "translate(100%)";
+		menu.classList.remove("showNavigation");
 		isMenuActive = false;
-		document.body.style.overflow = "auto";
 		animateCloseMenuButton(false);
 	}
 };
@@ -29,16 +27,16 @@ const toggleNavigation = () => {
 const showNavBarOnScroll = () => {
 	const scrollPosition = window.pageYOffset;
 
-	if (scrollPosition <= lastScroll && !linkClicked) {
+	if (scrollPosition <= lastScroll && !linkClicked && !isMenuActive) {
 		navBar.style.top = "0";
-		navBar.classList.add("header-active");
+		navBar.classList.add("navbar-active");
 		menuButton.classList.add("hamburger-menu-active");
 	} else {
 		navBar.style.top = "-20%";
 	}
 	if (scrollPosition === 0) {
 		navBar.style.top = "0";
-		navBar.classList.remove("header-active");
+		navBar.classList.remove("navbar-active");
 		menuButton.classList.remove("hamburger-menu-active");
 	}
 
@@ -82,9 +80,9 @@ navLinks.addEventListener("click", onNavigateHandler);
 socialIcons.addEventListener("click", toggleNavigation);
 window.addEventListener("scroll", showNavBarOnScroll);
 
-const myImgs = document.querySelectorAll(".animate");
-
-observer = new IntersectionObserver(
+// Fade-in content when scrolling
+const elementsToAnimate = document.querySelectorAll(".animate");
+const observer = new IntersectionObserver(
 	(entries) => {
 		entries.forEach((entry) => {
 			entry.target.classList.toggle("show", entry.isIntersecting);
@@ -97,7 +95,48 @@ observer = new IntersectionObserver(
 		threshold: 0.1,
 	}
 );
+elementsToAnimate.forEach((element) => {
+	observer.observe(element);
+});
 
-myImgs.forEach((image) => {
-	observer.observe(image);
+// Smooth scroll
+const scroll = (toElement, speed) => {
+	const windowObject = window;
+	let windowPosistion = windowObject.pageYOffset;
+	const pointer = toElement.getAttribute("href").slice(1);
+	const element = document.getElementById(pointer);
+	const elementOffset = element.offsetTop;
+
+	const counter = setInterval(() => {
+		if (windowPosistion > elementOffset) {
+			// from bottom to top
+			windowObject.scrollTo(0, windowPosistion);
+			windowPosistion -= speed;
+
+			if (windowPosistion <= elementOffset) {
+				// scrolling until elemOffset is higher than scrollbar position, cancel interval and set scrollbar to element position
+				clearInterval(counter);
+				windowObject.scrollTo(0, elementOffset);
+			}
+		} else {
+			// from top to bottom
+			windowObject.scrollTo(0, windowPosistion);
+			windowPosistion += speed;
+
+			if (windowPosistion >= elementOffset) {
+				// scroll until scrollbar is lower than element, cancel interval and set scrollbar to element position
+				clearInterval(counter);
+				windowObject.scrollTo(0, elementOffset);
+			}
+		}
+	}, 1);
+};
+
+const links = document.querySelectorAll('[data-identifier="link"]');
+
+links.forEach((link) => {
+	link.addEventListener("click", (event) => {
+		scroll(link, 30);
+		event.preventDefault();
+	});
 });
